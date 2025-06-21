c8a6f54d7c9aa4803c2179b70e786858e76a3270
Jinzhu WANG <wangjinzhulala@gmail.com>
2025-06-21 11:53:47 +1000

    update: dynamically load area data

--- components/chart_container.js
+++ components/chart_container.js
@@ -1,28 +1,26 @@
-
 const { mounted, beforeUnmount, props } = Vue;
 
-
 window.Highchart = {
-    props: {
-        chartOptions: {
-            type: Object,
-            required: true
-        },
-        chartData: {
-            type: Object,
-            required: true
-        }
+  props: {
+    chartOptions: {
+      type: Object,
+      required: true,
     },
-    mounted() {
-        this.chart = Highcharts.chart(this.$el, {
-            ...this.chartOptions,
-            series: this.chartData
-        });
+    chartData: {
+      type: Object,
+      required: true,
     },
-    beforeUnmount() {
-        if (this.chart) {
-            this.chart.destroy();
-        }
-    },
-    template: `<div style="height: 600px; margin-left: 300px;"></div>`
+  },
+  mounted() {
+    this.chart = Highcharts.chart(this.$el, {
+      ...this.chartOptions,
+      series: this.chartData,
+    });
+  },
+  beforeUnmount() {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+  },
+  template: `<div></div>`,
 };
--- views/Area.js
+++ views/Area.js
@@ -1,22 +1,50 @@
 window.AreaView = {
-    setup() {
-        // Access the global variables defined in area_1_total_area_wide_data.js
-        const area_1_total_area_wide_option = window.area_1_total_area_wide_option;
-        const area_1_total_area_wide_data = window.area_1_total_area_wide_data;
+  setup() {
+    const { ref } = Vue;
+    const isLoading = ref(true);
+    const chartOptions = ref(null);
+    const chartData = ref(null);
 
-        return {
-            // Expose them as reactive properties
-            area_1_total_area_wide_option,
-            area_1_total_area_wide_data
-        }
-    },
-    template: `
+    // Dynamically load the script when component is created
+    const loadScript = () => {
+      return new Promise((resolve, reject) => {
+        const script = document.createElement("script");
+        script.src = "./data/area_1_total_area_wide_data.js";
+        script.onload = () => {
+          chartOptions.value = window.area_1_total_area_wide_option;
+          chartData.value = window.area_1_total_area_wide_data;
+          isLoading.value = false;
+          resolve();
+        };
+        script.onerror = () => {
+          console.error("Failed to load area data");
+          isLoading.value = false;
+          reject();
+        };
+        document.head.appendChild(script);
+      });
+    };
+
+    // Load the script immediately when setup runs
+    loadScript();
+
+    return {
+      isLoading,
+      area_1_total_area_wide_option: chartOptions,
+      area_1_total_area_wide_data: chartData,
+    };
+  },
+  template: `
     <div class="p-6">
       <h1 class="text-2xl font-bold mb-6">Area Analysis</h1>
+      <div v-if="isLoading" class="flex justify-center items-center h-[400px]">
+        <div class="text-lg">Loading data...</div>
+      </div>
       <chart-container
+        v-else
         :chart-options="area_1_total_area_wide_option"
         :chart-data="area_1_total_area_wide_data">
       </chart-container>
     </div>
-  `
-}
+  `,
+};
